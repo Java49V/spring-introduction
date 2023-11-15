4961fef42a427a17e3880179ed22be349d3b12c7
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-11-15 13:16:22 +0300

    HW #59 save & restore functionality GreetingControllerTest

--- src/test/java/telran/spring/controller/GreetingsControllerTest.java
+++ src/test/java/telran/spring/controller/GreetingsControllerTest.java
@@ -1,14 +1,67 @@
 package telran.spring.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcBuilder;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import telran.spring.service.GreetingsService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class GreetingsControllerTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GreetingsService greetingsService;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new GreetingsController(greetingsService)).build();
+    }
+
+    @Test
+    void addPersonWithWrongEmail() throws Exception {
+        // Assume we have a JSON string for a person with wrong email format
+        String personJson = "{\"id\":1,\"name\":\"John\",\"city\":\"City\",\"email\":\"wrongemail\",\"phone\":\"+972-123-4567890\"}";
+        mockMvc.perform(post("/greetings/person")
+                .contentType("application/json")
+                .content(personJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    // Similar tests for wrong name, empty city, etc.
+
+    @Test
+    void updatePersonNormalFlow() throws Exception {
+        // Assume valid JSON string for updating a person
+        String personJson = "{\"id\":1,\"name\":\"John\",\"city\":\"City\",\"email\":\"john@example.com\",\"phone\":\"+972-123-4567890\"}";
+        mockMvc.perform(put("/greetings/person")
+                .contentType("application/json")
+                .content(personJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getPerson() throws Exception {
+        mockMvc.perform(get("/greetings/person/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deletePerson() throws Exception {
+        mockMvc.perform(delete("/greetings/person/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getGreetings() throws Exception {
+        mockMvc.perform(get("/greetings/1"))
+                .andExpect(status().isOk());
+    }
 
 }
